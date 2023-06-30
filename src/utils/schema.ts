2cdb4f0c690d1196d2f23944859fd31ace6a4acb
I Dewa Putu Semadi <dewasemadi3@gmail.com>
2023-06-30 21:23:37 +0700

    refactor: remove unused text field validation

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -3,6 +3,5 @@ import Joi from 'joi'
 export const collectionSchema = Joi.object({
   name: Joi.string().trim().required().messages({
     'string.empty': 'Collection name is required',
-    'string.pattern.base': 'Cannot contain spaces at the beginning or end',
   }),
 })
